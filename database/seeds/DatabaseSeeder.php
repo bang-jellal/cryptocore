f91fc8a369bf7d2f52891b5dc8e50e3968700cb1
Anwar Muthohari <anwar.muthohari@gmail.com>
2018-02-13 22:42:10 +0700

    Implement Login Page AdminLTE

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,6 +19,17 @@ class DatabaseSeeder extends Seeder
 
 
         // 2. DATA USERS
+        $admin = \App\Models\User::create([
+            'name'           => 'Admin',
+            'email'          => 'admin@mail.com',
+            'username'       => 'admin',
+            'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+        ]);
+        $admin_profile = factory(\App\Models\UserProfile::class)->make();
+        $admin->profile()->save($admin_profile);
+        $admin->assignRole('Admin');
+
         $users = factory(\App\Models\User::class)
             ->times(20)
             ->create()
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,69 +1,40 @@
-@extends('layouts.app')
+@extends('layouts.app_auth')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card card-default">
-                <div class="card-header">Login</div>
+    <p class="login-box-msg">Sign in to start your session</p>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+            <input type="email" id="email" class="form-control"
+                   placeholder="E-Mail Address" name="email" value="{{ old('email') }}">
+            @if ($errors->has('email'))
+                <span class="help-block">{{ $errors->first('email') }}</span>
+            @endif
+        </div>
+        <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+            <input type="password" id="password" class="form-control"
+                   placeholder="Password" name="password" value="{{ old('password') }}">
+            @if ($errors->has('password'))
+                <span class="help-block">{{ $errors->first('password') }}</span>
+            @endif
+        </div>
+        <div class="row">
+            <div class="col-xs-8">
+                <div class="checkbox icheck">
+                    <label>
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                    </label>
                 </div>
             </div>
+            <!-- /.col -->
+            <div class="col-xs-4">
+                <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            </div>
+            <!-- /.col -->
         </div>
-    </div>
-</div>
+    </form>
+
+    <a href="{{ route('password.request') }}">I forgot my password</a><br>
+    <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
 @endsection
