984f195a1469d663822a65ba2391b39f1383616e
anwar.muthohari <anwar.muthohari@gmail.com>
2018-02-14 14:57:50 +0700

    Implement Register Page AdminLTE

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -22,7 +22,6 @@ class DatabaseSeeder extends Seeder
         $admin = \App\Models\User::create([
             'name'           => 'Admin',
             'email'          => 'admin@mail.com',
-            'username'       => 'admin',
             'password'       => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
             'remember_token' => str_random(10),
         ]);
